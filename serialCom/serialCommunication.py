c680b82512aa272a077c9009f15909c1e5e5468b
Arvin-He <510205617@qq.com>
2017-05-09 18:30:33 +0800

    增加注释

--- serialCom/serialCommunication.py
+++ serialCom/serialCommunication.py
@@ -43,6 +43,8 @@ def hex2string():
 # 读到的数据"53 0B  01 01 04 30 02 B2 4A 58 09 6B 45",取其中的第8~11个16进制数
 # 然后将取到的16进制数作为一个整体,转换成10进制数,并返回
 def transformData(data):
+    # 串口读到的是字节
+    str = bytes.decode(encoding='utf-8', errors='strict)
     data_str = str(data)
     data_str.replace(" ", "")
     new_data_str = data_str[14:21]
