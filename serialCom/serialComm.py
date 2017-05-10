41d37278faac922bea445b28e1427165993e9a03
Arvin-He <510205617@qq.com>
2017-05-10 13:57:25 +0800

    update

--- serialCom/serialComm.py
+++ serialCom/serialComm.py
@@ -88,7 +88,7 @@ def saveCSV(data):
             # dialect为打开csv文件的方式，默认是excel，delimiter="\t"参数指写入的时候的分隔符
             writer = csv.writer(csv_file, dialect=("excel"))
             # csv文件插入一行数据，把下面列表中的每一项放入一个单元格（可以用循环插入多行）
-            head = ["序号", "标志位", "力矩", "角度"]
+            head = ["[Index]", "[Flag Bit]", "[Tight Torque]", "[Tight Angle]"]
             if head is not None:
                 writer.writerow(head)
             for row in data:
