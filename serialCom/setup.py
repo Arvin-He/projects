250f77b83cdaa8fd3802c3d39d01b8718c14c39d
Arvin-He <510205617@qq.com>
2017-05-10 17:40:41 +0800

    修改setup

--- serialCom/setup.py
+++ serialCom/setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-# setup(windows=['mainWindow.py'], options={"py2exe":{"includes":["sip"]}})
-setup(windows=['mainWindow.py'])
+setup(windows=['mainWindow.py'], options={"py2exe":{"includes":["sip"]}})
