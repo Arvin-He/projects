051371786797b911b3974ecf48e03bee0038d684
Arvin-He <510205617@qq.com>
2017-05-17 13:34:02 +0800

    将matplotlib控件嵌入到pyqt界面上

--- glassDetect/glassdetectdlg.py
+++ glassDetect/glassdetectdlg.py
@@ -5,8 +5,13 @@ import sys
 import time
 from datetime import datetime
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog, QSizePolicy
 from PyQt5.QtGui import QImage, QPixmap
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import random
 
 import utils
 import imgprocess
@@ -18,6 +23,27 @@ image_path = os.path.join(os.path.abspath("res"), "images")
 imageList = []
 
 
+class ImageCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        # self.axes = fig.add_subplot(111)
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+        self.toolbar = NavigationToolbar(self, parent)
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot()
+
+    def plot(self):
+        # data = [random.random() for i in range(25)]
+        # ax = self.figure.add_subplot(111)
+        # ax.plot(data, 'r-')
+        # ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+
+
 class GlassDetectDlg(QDialog, glassDetectDlg):
     def __init__(self, parent=None):
         super(GlassDetectDlg, self).__init__(parent)
@@ -27,9 +53,16 @@ class GlassDetectDlg(QDialog, glassDetectDlg):
         imageList = utils.getImageList(os.path.abspath(self.imagePathEdit.text()))
         print(imageList)
         src = imgprocess.loadImage(imageList[0])
-        self.showImage(src)
+        # self.showImage(src)
 
     def initUI(self):
+        # self.figure = Figure()
+        # self.canvas = FigureCanvas(self.figure)
+        self.canvas = ImageCanvas(self, width=5, height=4)
+        self.canvas.move(5, 40)
+        self.toolbar = self.canvas.toolbar
+        # self.addToolBar(self.toolbar)
+        self.imageViewLabel.hide()
         imagePath = utils.read_config(_config_path, "param", "image_path")
         self.imagePathEdit.setText(imagePath if imagePath else image_path)
         self.loadImagePathBtn.clicked.connect(self.on_loadImagePath)
@@ -38,6 +71,14 @@ class GlassDetectDlg(QDialog, glassDetectDlg):
         self.paramSettingBtn.clicked.connect(self.on_setParamEnable)
         self.upLimitEdit.editingFinished.connect(self.on_upLimitEdited)
         self.downLimitEdit.editingFinished.connect(self.on_downLimitEdited)
+
+    def plot(self):
+        ''' plot some random stuff '''
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.hold(False)
+        ax.plot(data, '*-')
+        self.canvas.draw()
 
     def on_loadImagePath(self):
         fileName, ok = QFileDialog.getOpenFileName(
@@ -53,7 +94,7 @@ class GlassDetectDlg(QDialog, glassDetectDlg):
         print(imageList)
 
     def on_prevImage(self):
-        pass
+        self.plot()
 
     def on_nextImage(self):
         pass
