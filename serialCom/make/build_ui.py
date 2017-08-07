db4ed770ad43a5f1f809d1aa67a8f87670b18e37
Arvin-He <510205617@qq.com>
2017-08-07 17:49:55 +0800

    update

--- serialCom/make/build_ui.py
+++ serialCom/make/build_ui.py
@@ -1,17 +1,18 @@
 # -*- coding:utf-8 -*-
 
 import os
-import subprocess
 import sys
+import subprocess
+
 
 inputFile = os.path.abspath(os.path.join("../res", "serialCom.ui"))
 print("input file ={}".format(inputFile))
 outputFile = os.path.abspath("../serialCom_ui.py")
 print("output file ={}".format(outputFile))
-# py3.4.3
 try:
+    # py3.4.3
     subprocess.call(["pyuic5.bat", inputFile, "-o", outputFile])
 except:
-# py3.6
+    # py3.6
     subprocess.call(["pyuic5", inputFile, "-o", outputFile])
 print("build ui done.")
