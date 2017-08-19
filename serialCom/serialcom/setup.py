7883a77e8dfc865cb16481b352a591fc65a48973
Arvin-He <510205617@qq.com>
2017-08-19 08:30:12 +0800

    update

--- serialCom/serialcom/setup.py
+++ serialCom/serialcom/setup.py
@@ -6,12 +6,12 @@ from cx_Freeze import setup, Executable
 
 
 local_files = []
-if os.path.exists("log/"):
-    local_files.append("log/")
-elif os.path.exists("userdata/"):
-    local_files.append("userdata/")
-elif os.path.exists("config/"):
-    local_files.append("config/")
+# if os.path.exists("log/"):
+#     local_files.append("log/")
+# elif os.path.exists("userdata/"):
+#     local_files.append("userdata/")
+# elif os.path.exists("config/"):
+#     local_files.append("config/")
 
 
 # 依赖会自动检测,但会需要微调
